170da783caad779220d7b55204ad95154174264c
Rahultech-1999 <138772772+rahultech-1999@users.noreply.github.com>
2024-01-05 20:44:49 +0530

    Add files via upload

---
+++ To-Do App/todo-app-yt-backend-main/todo-app-yt-backend-main/Server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/ToDoRoutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://rahul20587:AHHBql6aMtbt5Tn5@cluster0.gjlpcjq.mongodb.net/TO-DO_DB?retryWrites=true&w=majority')
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
